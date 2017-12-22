d301e8c2502d6ff72abf60ad1208e8b617be63a5
zhaopengyun <zhao942548838@qq.com>
2017-12-22 18:29:02 +0800

    update

---
+++ example/__init__.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+# @Created on    : 17/12/22 下午4:21
+# @Author  : zpy
+# @Software: PyCharm
---
+++ example/test.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+# @Created on    : 17/12/22 下午4:21
+# @Author  : zpy
+# @Software: PyCharm
+
+from sp_except import SpiderException
+
+
+raise SpiderException(2, 'fx')
