e4221d406e5c00aae653ab94e2b1234885ab8c08
zhao94254 <zhao942548838@qq.com>
2018-01-01 00:15:07 +0800

    update

---
+++ server/test.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# @Author  : pengyun
+
+
+import requests
+
+
+data = {
+    ''
+}
+requests.post('127.0.0.1:5000', )
--- server/utils.py
+++ server/utils.py
@@ -3,6 +3,16 @@
 
 from collections import namedtuple
 
+def p_named(named):
+    """
+    打印named 中的属性。
+    :param named:
+    :return:
+    """
+    for i in dir(named):
+        if not i.startswith('_'):
+            print(i)
+
 def convert(d):
     """ web请求 --》 convert --》framework --》 work --》response"""
     n = namedtuple(d.pop('name'), ['url'])
@@ -11,3 +21,14 @@ def convert(d):
         j = 'handler_' + i
         setattr(n, j, d[i])
     return n
+
+if __name__ == '__main__':
+    data = {
+        'url':'xxx', # 爬取的url
+        'name': 'xxx', # 项目名
+        # 要获取的值 对应的是xpath 或者 re 或者。。
+        'title': 'xxxxx',
+        'describe': 'xxxxxx',
+    }
+    d = convert(data)
+    p_named(d)
